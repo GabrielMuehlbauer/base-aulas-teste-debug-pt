4e230621f793ec5b80f2c3ebeda364a633bad908
Gabriel Muehlbauer <muehlbauergabriel@gmail.com>
2026-06-12 15:17:18 -0300

    Adição da planilha de Caso de Testes e implementação de testes automatizados

--- src/services/exercicios.js
+++ src/services/exercicios.js
@@ -1,18 +1,50 @@
 class Service {
     Somar(num1, num2) {
-        return num1 - num2;
+        if (num1 === undefined || num2 === undefined) {
+            return "Envie todos os campos!";
+        }
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2)) {
+            return "Enviar somente números";
+        }
+
+        return num1 + num2;
     }
 
     Subtrair(num1, num2) {
+        if (num1 === undefined || num2 === undefined) {
+            return "Envie todos os campos!";
+        }
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2)) {
+            return "Enviar somente números";
+        }
+
         return num1 - num2;
     }
 
     Multiplicar(num1, num2) {
+        if (num1 === undefined || num2 === undefined) {
+            return "Envie todos os campos!";
+        }
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2)) {
+            return "Enviar somente números";
+        }
+
         return num1 * num2;
     }
 
     Dividir(num1, num2) {
-        return num2 / num1;
+        if (num1 === undefined || num2 === undefined) {
+            return "Envie todos os campos!";
+        }
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2)) {
+            return "Enviar somente números";
+        }
+
+        if (num2 === 0) {
+            return "Undefined";
+        }
+
+        return num1 / num2;
     }
 }
 
